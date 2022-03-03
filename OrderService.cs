daad56f48cebb76723b9f248171b9ffbfab5f1c0
rominamenacho <43242020+rominamenacho@users.noreply.github.com>
2022-03-03 00:25:45 -0300

    first commit

---
+++ OrderService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsabilityPrinciple_SOLID_
+{
+    //clase que permite una orden de compra y luego manda notificacion al cliente
+    class OrderService
+    {
+        private readonly System.Net.Mail.SmtpClient _smtpClient;
+
+            public OrderService(SmtpClient smtpClient)
+            {
+                _smtpClient = smtpClient;
+            }
+
+        //tarea 1
+        public async Task Add(Order order) {
+            //1 cod para crear la orden
+
+            //2 notificar
+            var message = new MailMessage("sales@admin.com", order.ClientEmail)
+            {
+                Subject = "Se le asigno una compra",
+                Body = "estimado, hemos creado su nueva orden de compra"
+            };
+            await this.SendCustomerNotification(message);
+        }
+
+
+
+        //tarea 2
+        public async Task SendCustomerNotification(MailMessage message) {
+            await _smtpClient.SendMailAsync(message);
+        }
+
+    }
+
+    public class Order
+    {
+        public string ClientEmail { get; internal set; }
+    }
+}
---
+++ OrderServiceRefactor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsabilityPrinciple_SOLID_
+{
+    class OrderServiceRefactor
+    {
+        private readonly NotificationService _notificationService;
+
+        public OrderServiceRefactor(NotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task Add(Order order)
+        {
+            // crear orden
+
+            //notificar
+            var message = new MailMessage("sales@admin.com", order.ClientEmail)
+            {
+                Subject = "Se le asigno una compra",
+                Body = "estimado, hemos creado su nueva orden de compra"
+            };
+
+            await this._notificationService.SendEmail(message);
+        }
+
+    }
+
+    //la tarea de enviar la notif se realiza en otra clase
+
+    public class NotificationService
+    {
+        private readonly System.Net.Mail.SmtpClient _smtpClient;
+
+        public NotificationService(SmtpClient smtpClient)
+        {
+            _smtpClient = smtpClient;
+        }
+
+
+        public async Task SendEmail(MailMessage message)
+        {
+            await _smtpClient.SendMailAsync(message);
+        }
+    }
+
+}
